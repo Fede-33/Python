0124f729c3520d88d503a273e5379c71c1e879c4
Fede-33 <pereyra.federico@ymail.com>
2024-04-11 20:30:18 -0300

    finalizado con ejemplo calculadora

---
+++ Curso MsBuild/01Intro/Calculadora.py
@@ -0,0 +1,17 @@
+num1 = float(input('Ingrese primer número: '))
+num2 = float(input('Ingrese segundo número: '))
+oper = float(input('Seleccione operación: Suma(1) - Resta(2) - Producto (3) - Cociente(4) '))
+
+if oper >= 1 and oper <= 4:
+    if oper == 1:
+        num3 = num1+num2
+    elif oper == 2:
+        num3 = num1-num2
+    elif oper == 3:
+        num3 = num1*num2
+    elif oper == 4:
+        num3 = num1/num2
+
+    print('El resultado es: '+ str(num3))
+else : print('Operación incorrecta')
+
