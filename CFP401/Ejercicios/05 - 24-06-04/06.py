198a7f8013cceb6dc291ae576afde329d602284d
Fede-33 <112910064+Fede-33@users.noreply.github.com>
2024-06-07 22:55:34 -0300

    Actualizar 06.py

--- CFP401/Ejercicios/05 - 24-06-04/06.py
+++ CFP401/Ejercicios/05 - 24-06-04/06.py
@@ -6,3 +6,17 @@
 #"pan": 2.25, 
 #"huevos": 2.0 }
 #calcule el monto total a pagar.
+
+carrito_compras = {
+    "manzanas": 2.5, 
+    "bananas": 1.75, 
+    "leche": 3.0, 
+    "pan": 2.25, 
+    "huevos": 2.0
+}
+total = 0
+
+for i in carrito_compras.values():
+    total += i
+
+print(f'El total de los productos es {total}')
